9b888c85825e5577135cb883d89e9b283ff25947
nikadolidze <nikadolidze@lemondo.com>
2023-06-07 23:57:56 +0400

    Create Product.js

---
+++ src/Pages/shop/Product.js
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react' 
+import { ShopContext } from './../../context/Shop-Context';
+
+
+ const Product = (props) => {
+    const {id, productName, price, productImage}=props.data;
+    const {addToCart, cartItems}=useContext(ShopContext);
+
+    const cartItemAmount=cartItems[id]
+ 
+    return (
+      <div className="product">
+        <img src={productImage} alt="img" />
+        <div className="description">
+            <p>
+                <b>{productName}</b>
+            </p>
+            <p>  
+                $ {price}
+            </p>
+        </div>
+           <button className="addToCartBttn"  
+            onClick={()=>addToCart(id)}>Add To Cart
+            {cartItemAmount > 0 && <>({cartItemAmount})</>}
+            </button>
+           
+      </div>
+  )
+}
+
+export default Product
